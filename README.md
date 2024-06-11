50c63c8674aa58cc0fc6e8b1505de02bdfbd9d44
Kauã Morais <kaua.morais22@escola.pr.gov.br>
2024-06-11 09:35:21 -0300

    okkk

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# advinhacao
+# Comantos Git 
+
+git add -A    adiciona todos os arquivos ao git
+
+git commit -m "texto"   comenta os arquivos adicionados
+
+git pull    puxa os arquivos do git
+
+git push    envia ao git os arquivos e comentarios 
+
+
---
+++ jogoadvinha.js
@@ -0,0 +1,31 @@
+const min = 100;
+const max = 200;
+const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+let tentativas = 0;
+const maxtentativas = 5;
+
+process.stdout.write('Adivinhe o número entre', min, 'e', max,': ');
+
+process.stdin.on('data', (input) => {
+  const entrada = parseInt(input.toString().trim());
+
+  if (isNaN(entrada) || entrada < min || entrada > max) {
+    process.stdout.write('Por favor, insira um número válido entre', min, 'e', max,': ');
+  } else {
+    tentativas++;
+
+    if (entrada === randomNum) {
+      process.stdout.write(`Parabéns! Você acertou o número ${randomNum} em ${tentativas} tentativas.\n`);
+      process.exit();
+    } else {
+      process.stdout.write('Você errou! Tente novamente.\n');
+      if (tentativas === maxtentativas) {
+        process.stdout.write(`Você atingiu o número máximo de tentativas (${maxtentativas}). O número era ${randomNum}.\n`);
+        process.exit();
+      } else {
+        process.stdout.write(`Tentativa ${tentativas} de ${maxtentativas}.\n`);
+        process.stdout.write('Adivinhe o número entre', min, 'e', max,': ');
+      }
+    }
+  }
+});
