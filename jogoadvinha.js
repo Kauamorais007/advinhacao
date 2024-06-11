d719f5743af1e0197cd75610536564b34d28043c
Kauã Morais <kaua.morais22@escola.pr.gov.br>
2024-06-11 09:39:10 -0300

    Jogo ok e branch dev ok

--- jogoadvinha.js
+++ jogoadvinha.js
@@ -1,6 +1,6 @@
 const min = 100;
 const max = 200;
-const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+const numerorandom = Math.floor(Math.random() * (max - min + 1)) + min;
 let tentativas = 0;
 const maxtentativas = 5;
 
@@ -14,13 +14,13 @@ process.stdin.on('data', (input) => {
   } else {
     tentativas++;
 
-    if (entrada === randomNum) {
-      process.stdout.write(`Parabéns! Você acertou o número ${randomNum} em ${tentativas} tentativas.\n`);
+    if (entrada === numerorandom) {
+      process.stdout.write(`Parabéns! Você acertou o número ${numerorandom} em ${tentativas} tentativas.\n`);
       process.exit();
     } else {
       process.stdout.write('Você errou! Tente novamente.\n');
       if (tentativas === maxtentativas) {
-        process.stdout.write(`Você atingiu o número máximo de tentativas (${maxtentativas}). O número era ${randomNum}.\n`);
+        process.stdout.write(`Você atingiu o número máximo de tentativas (${maxtentativas}). O número era ${numerorandom}.\n`);
         process.exit();
       } else {
         process.stdout.write(`Tentativa ${tentativas} de ${maxtentativas}.\n`);
